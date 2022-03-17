4fc753c1bbe55c38acd7c77efa84f66138fcec83
brandonvicc <bvicc93@gmail.com>
2022-03-17 12:08:45 -0500

    delete on homepage functionality works

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -4,14 +4,17 @@ import * as businessActions from "../../store/business";
 import { useDispatch, useSelector } from "react-redux";
 import HomeHero from "./HomeHero";
 import Footer from "../Footer/Footer";
+import BusinessCard from "../BusinessCard/BusinessCard";
 
 const HomePage = () => {
-  const dispatch = useDispatch();
   const businesses = useSelector((state) => state.business.businesses);
+  const dispatch = useDispatch();
+
   console.log(businesses);
   useEffect(() => {
     dispatch(businessActions.getAll());
   }, [dispatch]);
+
   return (
     <div className="home-container">
       <HomeHero />
@@ -21,19 +24,7 @@ const HomePage = () => {
         </h1>
         <ul className="home-business-list">
           {businesses?.map((business) => (
-            <li className="home-business-card" key={business.id}>
-              <h3 className="home-business-card-title">{business.name}</h3>
-              <div className="home-business-img-container">
-                <img
-                  className="home-business-img"
-                  src={business.img_link}
-                  alt="business"
-                />
-              </div>
-              <div className="home-business-card-content">
-                <p>Rating: {business.avg_review}</p>
-              </div>
-            </li>
+            <BusinessCard business={business} key={business.id} />
           ))}
         </ul>
       </div>
