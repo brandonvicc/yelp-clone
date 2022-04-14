33928b43cf38199a58c0941d2e39e59e4378ec17
brandonvicc <bvicc93@gmail.com>
2022-04-14 10:26:33 -0500

    google maps bug

--- backend/routes/api/maps.js
+++ backend/routes/api/maps.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const { googleMapsAPIKey } = require("../../config");
 
 router.post("/key", (req, res) => {
+  console.log("request key");
   res.json({ googleMapsAPIKey });
 });
 
